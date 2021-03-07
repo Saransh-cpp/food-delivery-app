302557fdfc0fd7bb1fa441e80a9f8f14e7e0199a
Saransh <saransh0701@gmail.com>
2021-03-07 21:36:04 +0530

    Created utils

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,18 +1,7 @@
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import firebase from "firebase";
 import { useEffect } from "react";
+import firebase from '../utils/Firebase'
 
-const config = {
-  apiKey: "AIzaSyDtS6oZab6YAdD3MXr4jCzB7WuLVYkxRKk",
-  authDomain: "fooddeliveryapp-7848a.firebaseapp.com",
-  projectId: "fooddeliveryapp-7848a",
-  storageBucket: "fooddeliveryapp-7848a.appspot.com",
-  messagingSenderId: "207856320422",
-  appId: "1:207856320422:web:25b49a642d91aee5c5d967",
-  measurementId: "G-CKNT4QBHNZ",
-};
-
-firebase.initializeApp(config);
 
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
---
+++ src/utils/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyDtS6oZab6YAdD3MXr4jCzB7WuLVYkxRKk",
+  authDomain: "fooddeliveryapp-7848a.firebaseapp.com",
+  projectId: "fooddeliveryapp-7848a",
+  storageBucket: "fooddeliveryapp-7848a.appspot.com",
+  messagingSenderId: "207856320422",
+  appId: "1:207856320422:web:25b49a642d91aee5c5d967",
+  measurementId: "G-CKNT4QBHNZ",
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
